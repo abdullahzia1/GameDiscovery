fa2bb2f84e01d765ee47f7f0817a5aef50268066
Abdullah Zia <abziawork960@gmail.com>
2024-03-10 16:31:57 +0500

    replaced game hook with custom hook

--- frontend/src/components/GameGrid.tsx
+++ frontend/src/components/GameGrid.tsx
@@ -6,16 +6,17 @@ import GameCardContainer from "./GameCardContainer";
 import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={10}
-        spacing={10}
+        spacing={8}
+        justifyContent={"center"}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -23,7 +24,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
