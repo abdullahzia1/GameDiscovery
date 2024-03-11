3cd36fa5964a049d95dba4ddc07329058ef9ceba
Abdullah Zia <abziawork960@gmail.com>
2024-03-11 14:09:10 +0500

    added platform component

--- frontend/src/components/PlatformSelector.tsx
+++ frontend/src/components/PlatformSelector.tsx
@@ -1,19 +1,33 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatform from "../hooks/usePlatform";
+import { Platform } from "../hooks/useGames";
 
-const PlatformSelector = () => {
+interface Props {
+  selectedPlatform: (platform: Platform | null) => void;
+  selectDisplayPlatform: Platform | null;
+}
+
+const PlatformSelector = ({
+  selectedPlatform,
+  selectDisplayPlatform,
+}: Props) => {
   const { data, error } = usePlatform();
 
   if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platform
+        {selectDisplayPlatform?.name || "Platform"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            onClick={() => selectedPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
